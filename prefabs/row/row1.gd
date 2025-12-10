51e6a923f22653b4d952c0d1d4f7f0ba7a5afb97
FrankieMundo-MohawkCollege <frank.mundo3@mohawkcollege.ca>
2025-12-09 23:37:17 -0500

    Made Pegs Move and also Made Diamonds into one big Rectangle.

---
+++ prefabs/row/row1.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+var speed = 60
+var move_range = 40
+var start_x = 0
+
+func _ready():
+	start_x = position.x
+
+func _process(delta):
+	position.x = start_x + sin(Time.get_ticks_msec() / 600.0) * move_range
